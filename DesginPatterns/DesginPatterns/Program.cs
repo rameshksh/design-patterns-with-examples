2ddda3401b51a313f22193ee2803ea516e2fc2e4
rameshksh <ramesh.sharma12@gmail.com>
2018-07-30 18:08:45 +0530

    added solution

---
+++ DesginPatterns/DesginPatterns/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DesginPatterns
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
